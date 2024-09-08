bff54bf65100a1f802f6e6016244bf396f34c060
Sterlites <101171175+Sterlites@users.noreply.github.com>
2024-09-08 23:42:16 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
