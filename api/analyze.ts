98dd5ab3077dcf0fba5de6850f5d0601dc071a81
mcsdevv <mail@mcs.dev>
2024-09-12 08:45:23 +0100

    add back file extension

--- api/analyze.ts
+++ api/analyze.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { analyzeWebsite } from "./utils/_analyzeWebsite";
+import { analyzeWebsite } from "./utils/_analyzeWebsite.ts";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // CORS headers
